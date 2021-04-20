33c0655e0069d9a02744d09ecc5ec6ec6606da4e
m0n411111 <bjn.ant1@gmail.com>
2021-04-20 11:12:57 +0200

    fin

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,5 +1,18 @@
 def stock_picker(array)
-    
+    best_days = [0,0]
+    profit = 0
+
+    array.each_with_index do |buy_price, buy_day|
+        array.each_with_index do |sell_price, sell_day|
+
+            if sell_day >= buy_day && (sell_price - buy_price) > profit
+              profit = sell_price - buy_price
+              best_days[0] = buy_day
+              best_days[1] = sell_day
+            end
+        end
+    end
+    best_days
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
